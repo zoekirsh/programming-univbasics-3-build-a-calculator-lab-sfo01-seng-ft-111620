adbf3451b36de48920baee110db4fd47468ded76
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-01 23:54:04 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,21 +5,31 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  difference = num1 - num2
+  puts difference
+  difference
 end
 
 def division(num1, num2)
-
+  quotient = num1 / num2
+  puts quotient
+  quotient
 end
 
 def multiplication(num1, num2)
-
+  multiple = num1 * num2
+  puts multiple
+  multiple
 end
 
 def modulo(num1, num2)
-
+  remainder = num1 % num2
+  puts remainder
+  remainder
 end
 
 def square_root(num)
-
+  root = Math.sqrt(num)
+  puts root
+  root
 end
